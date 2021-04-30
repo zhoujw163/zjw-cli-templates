34185c0c9e892da0d4f0e04c2c1ec7f847cfd14f
zhoujiawei <zhoujiawei@nayuan.net>
2021-04-30 16:11:22 +0800

    feat: 增加自定义模板

---
+++ zjw-cli-template-custom-vue2/template/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    vue2 custom template
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
